b025b278ca40f769f562c5367621c17c281f776a
Learner <jagdeepgupta813@gmail.com>
2020-05-23 14:47:48 -0700

    trying first commit for CI cd

---
+++ src/main/java/com/sample/maven/java/Application.java
@@ -0,0 +1,8 @@
+package com.sample.maven.java;
+
+public class Application {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println("2 + 2 = " + adder.add(2, 2));
+    }
+}
